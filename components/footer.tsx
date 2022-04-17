01fcca59f73b01ce3e35f33d6ab97355f19ac498
Seimei Matsusaki <seimatsusaki@gmail.com>
2022-04-17 13:54:41 -0400

    Added sharp

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,6 @@ export default function Footer () {
   const animeClass = style.appear_and_fadeout;
   const popupRef = useRef<HTMLSpanElement>(null);
   const onClickEmailAddress = (e: any) => {
-        console.log('clicked')
         e.stopPropagation();
         navigator.clipboard.writeText(emailAddress);
         popupRef?.current?.classList.add(animeClass);
@@ -22,7 +21,6 @@ export default function Footer () {
   return (
     <footer className={style.section_wrapper} id='contact'>
       <div className={style.content_wrapper}>
-        {/* <h2 className={style.main_header}><span className={style.main_header_letter}>CONTACT</span></h2> */}
         <p className={style.email}>
           <span className={style.email_letters} 
             onClick={onClickEmailAddress}>{emailAddress}
@@ -33,18 +31,18 @@ export default function Footer () {
       <div className={style.image_wrapper}>
         <a href='https://github.com/Lulkafe' className={style.sns_anchor}>
           <Image
+            src={'/Github-Mark-120px-plus.png'}
             width={32}
             height={32}
             alt={'Github icon'}
-            src={'/Github-Mark-120px-plus.png'}
           />
         </a>
         <a href='https://www.linkedin.com/in/smatsusaki/' className={style.sns_anchor}>
           <Image
+            src={'/linkedin-icon.png'}
             width={32}
             height={32}
             alt={'Linkedin icon'}
-            src={'/linkedin-icon.png'}
           />
          </a>
       </div>
